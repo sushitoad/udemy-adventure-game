a5dbb980db72009399358614b0d02d5e24febe83
quillenink <36284902+sushitoad@users.noreply.github.com>
2025-05-23 16:53:07 -0700

    block puzzle

--- scripts/locked_door.gd
+++ scripts/locked_door.gd
@@ -1,11 +1,16 @@
 extends StaticBody2D
 
+var buttonsPressed: int = 0
+@export var buttonsToOpen: int = 1
 
 func _on_puzzle_button_pressed() -> void:
-	visible = false
-	$CollisionShape2D.set_deferred("disabled", true)
-
+	buttonsPressed += 1
+	if buttonsPressed >= buttonsToOpen:
+		visible = false
+		$CollisionShape2D.set_deferred("disabled", true)
 
 func _on_puzzle_button_unpressed() -> void:
-	visible = true
-	$CollisionShape2D.set_deferred("disabled", false)
+	buttonsPressed -= 1
+	if buttonsPressed < buttonsToOpen:
+		visible = true
+		$CollisionShape2D.set_deferred("disabled", false)
