c28c137c460d520190f39b37bb7b04f3b3b451b2
quillenink <36284902+sushitoad@users.noreply.github.com>
2025-08-01 17:39:45 -0700

    ep 38 npc and messing around

---
+++ scripts/npc.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+var player: CharacterBody2D
+@export var talkDistance: float
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Interact"):
+		var distance = transform.origin.distance_to(player.transform.origin)
+		if distance <= talkDistance:
+			print("Hello traveler!")
