9ebdc4fd918da6bfab1d1dd0c6bc569b27ab2b8f
quillenink <36284902+sushitoad@users.noreply.github.com>
2025-08-04 16:35:55 -0700

    ep 39 dialogue box

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -5,10 +5,14 @@ var player: CharacterBody2D
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
+	$CanvasLayer.visible = false
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Interact"):
 		var distance = transform.origin.distance_to(player.transform.origin)
-		#print(distance)
 		if distance <= talkDistance:
+			Talk("nothing")
 			print("Hello traveler!")
+
+func Talk(words: String):
+	$CanvasLayer.visible = !$CanvasLayer.visible
