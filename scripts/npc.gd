792a34dd1cf6a77030b0efa12742cf163c59cf9e
quillenink <36284902+sushitoad@users.noreply.github.com>
2025-08-06 15:40:49 -0700

    ep 41

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -1,10 +1,12 @@
 extends StaticBody2D
 
 var playerInRange: bool = false
+@export var characterName: String
 @export var sentences: String
 
 func _ready() -> void:
 	StopTalking()
+	$CanvasLayer/CharacterName.text = characterName
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Interact") and playerInRange:
