2031d854aa1d3a857e470a5531f0856b36faae98
quillenink <36284902+sushitoad@users.noreply.github.com>
2025-08-18 17:18:05 -0700

    ep 50
    you also added your own animation to the scroll

---
+++ scripts/treasure_chest.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+
+var playerInRange: bool = false
+@onready var sprite = $AnimatedSprite2D
+@onready var scrollTimer = $Timer
+@onready var scrollSprite = $Sprite2D
+
+var opened: bool = false
+
+func _ready() -> void:
+	if !opened:
+		sprite.play("closed")
+	else:
+		sprite.play("open")
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Interact") and playerInRange:
+		if !opened:
+			OpenChest()
+			
+func OpenChest():
+	sprite.play("open")
+	opened = true
+	scrollTimer.start()
+	$AnimationPlayer.play("collectScroll")
