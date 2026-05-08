0a9cee77f5e96d755fd5e00e2d58483748615588
quillenink <36284902+sushitoad@users.noreply.github.com>
2026-05-08 10:10:57 -0700

    ep 59 8:00

--- scripts/slime_enemy.gd
+++ scripts/slime_enemy.gd
@@ -1,10 +1,19 @@
 extends CharacterBody2D
 
 @export var maxHP: int = 1
+@export var moveSpeed: float = 30
 var currentHP: int
+var target: Node2D
 
 func _ready() -> void:
 	currentHP = maxHP
+
+func _physics_process(delta: float) -> void:
+	if target:
+		var distance: Vector2 = (target.global_position - global_position).normalized()
+		velocity = distance * moveSpeed
+	
+	move_and_slide()
 
 func TakeDamage(damage: int):
 	currentHP -= damage
@@ -13,3 +22,7 @@ func TakeDamage(damage: int):
 
 func _on_death_sound_finished() -> void:
 	call_deferred("queue_free")
+
+func _on_los_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		target = body
