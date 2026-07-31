cddbeded3e3c0a10e747873344946b72234b98e6
sushitoad <36284902+sushitoad@users.noreply.github.com>
2026-07-31 10:55:26 -0700

    shrines work fuck yeah
    work just fine through different scenes as long as I keep a list of the game scenes in scene manager which is fine

--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -3,10 +3,10 @@ extends Node2D
 var spawnPoint: Vector2
 var spawnPlayerFacing: String
 #reference to the position the player should spawn in if dead
-var activeShrinePosition
+var activeShrinePosition: Vector2
 ##reference to the index from the gameSceneList array that the player should spawn into
-var shrineSceneIndex
-var sceneToLoadOnRespawn: PackedScene
+var shrineSceneIndex: int
+var gameSceneList: Array[PackedScene] = [preload("res://scenes/game_scene.tscn"), preload("res://scenes/cave_scene.tscn")]
 
 var playerCollectedHearts: int = 3
 
@@ -16,13 +16,8 @@ var openedChests: Array[String] = []
 
 func _ready() -> void:
 	playerCurrentHP = playerCollectedHearts * 2
-	#spawnPoint = find_node_in_scene("Player").position
 
 func RespawnAtNearestShrine():
-	#load the scene that matches the shrineSceneIndex in the array of scenes
-	get_tree().change_scene_to_packed(sceneToLoadOnRespawn)
-	#set the player character to activeShrinePosition
-	var player: Node2D = get_tree().get_first_node_in_group("player")
-	player.global_position = activeShrinePosition
-	#set them to full health
+	get_tree().change_scene_to_packed(gameSceneList[shrineSceneIndex])
+	spawnPoint = activeShrinePosition
 	playerCurrentHP = playerCollectedHearts * 2
